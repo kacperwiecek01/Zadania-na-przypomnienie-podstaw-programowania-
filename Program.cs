e674a70e43f396bb70db483a5293c890a1250c96
kacperwiecek01 <kacper.wiecek@student.wsb.edu.pl>
2025-04-21 12:31:56 +0200

    Update Program.cs
    Dodane kalkulator dwóch liczb

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.Write("Podaj pierwsza liczbe: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj druga liczbe: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Wybierz operację (+, -, *, /): ");
+        string operacja = Console.ReadLine();
+
+        double wynik;
+
+        if ( operacja == "+")
+        {
+            wynik = a + b;
+            Console.WriteLine("Wynik: " + wynik);
+
+        }
+        else if ( operacja == "-" )
+        {
+            wynik = a - b;
+            Console.WriteLine("Wynik: " + wynik);
+        }
+        else if ( operacja == "*" )
+        {
+            wynik = a * b;
+            Console.WriteLine("Wynik: " + wynik);
+        }
+        else if ( operacja == "/" )
+        {
+           if ( b != 0 )
+            {
+                wynik = a / b;
+                Console.WriteLine("Wynik: " + wynik);
+            }
+           else
+            {
+                Console.WriteLine("Błąd: Nie można dzielić przez zero!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Błąd: Nieznana operacja!");
+        }
+    }
+}
