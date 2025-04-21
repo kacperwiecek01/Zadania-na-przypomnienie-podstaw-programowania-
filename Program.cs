bb449348bd194a85f5ca678f95ed89a745135eff
kacperwiecek01 <kacper.wiecek@student.wsb.edu.pl>
2025-04-21 13:00:24 +0200

    Update Program.cs
    Dodane konwenter temperatur oraz menu wyboru programu / zadania.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,38 @@ class Program
 
     static void Main()
     {
+        bool Kontynuuj = true;
+
+        while(Kontynuuj)
+        {
+            Console.WriteLine("\n --Menu--");
+            Console.WriteLine("1. Kalkulator dwóch liczb");
+            Console.WriteLine("2. Konwenter temperatur");
+            Console.WriteLine("0. Wyjście");
+            Console.Write("Wybierz zadanie: ");
+            string wybor = Console.ReadLine();
+
+            switch (wybor)
+            {
+                case "1":
+                    Kalkulator();
+                    break;
+                case "2":
+                    KonwenterTemperatur();
+                    break;
+                case "0":
+                    Kontynuuj = false;
+                    break;
+                default:
+                    Console.WriteLine("Nieprawidłowy wybór.");
+                    break;
+
+            }
+        }    
+    }
+    
+    static void Kalkulator()
+    {
         Console.Write("Podaj pierwsza liczbe: ");
         double a = Convert.ToDouble(Console.ReadLine());
 
@@ -48,6 +80,32 @@ class Program
         else
         {
             Console.WriteLine("Błąd: Nieznana operacja!");
+        }
+    }
+
+    static void KonwenterTemperatur()
+    {
+        Console.Write("Wybierz Konwersję (C- Celsujsz -> Fahrenheit, F - Farhenheit -> Celsjusz): ");
+        string kierunek = Console.ReadLine().ToUpper();
+
+        Console.Write("Podaj Temperaturę: ");
+        double temperatura = Convert.ToDouble(Console.ReadLine());
+
+        double wynik;
+
+        if (kierunek == "C")
+        {
+            wynik = temperatura * 1.8 + 32;
+            Console.WriteLine($"{temperatura}°C = {wynik}°F");
+        }
+        else if ( kierunek == "F" )
+        {
+            wynik = (temperatura - 32) / 1.8;
+            Console.WriteLine($"{temperatura}°F = {wynik}°C");
+        }
+        else
+        {
+             Console.WriteLine("Błąd: Nieznany tryb konwersji ( Użyj C lub F )");
         }
     }
 }
